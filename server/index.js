e67ac5fa057c3f72e352aecbb799a6fc1c9385cb
wombat1972 <53923403+wombat1972@users.noreply.github.com>
2020-05-23 15:49:20 -0400

    app started

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+app.listen(5000, () => {
+  console.log("we be rockin on 5000");
+});
